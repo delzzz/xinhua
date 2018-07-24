cb760fc1fe890b13b078e72d1b2200b936ef1232
diane <1428319760@qq.com>
2018-07-24 11:07:14 +0800

    打印日志

--- app/Http/Controllers/Frontend/UserController.php
+++ app/Http/Controllers/Frontend/UserController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use App\UserWeixin;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Cache as C;
-use App\Log;
+use Illuminate\Support\Facades\Log;
 
 
 
